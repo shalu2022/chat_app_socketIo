bca238008dfc331bfb114f60705a067c888176a1
shalu2022 <frontendstuff2022@gmail.com>
2023-09-18 23:54:23 +0530

    saving current user into Redux and added new room in UI

---
+++ client/src/services/room.service.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const baseUrl = `${process.env.REACT_APP_BASE_URL}`
+class roomService {
+    async addRoom(payload){
+        try{
+            // const response = axios(`${baseUrl}/addRoom`, roomDetails)
+            // return response
+            console.log("room details", payload);
+        }catch (err){
+            throw err.message
+        }
+    }
+}
+
+export default new roomService();
