0a73e7f81adbcd7f49fa86036a623b7c400fad8e
shalu2022 <frontendstuff2022@gmail.com>
2023-09-22 00:05:52 +0530

    add new room functionality added and rooms list coming from be

--- client/src/services/room.service.js
+++ client/src/services/room.service.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
-const baseUrl = `${process.env.REACT_APP_BASE_URL}`
+import axios from "axios";
+const baseUrl = `${process.env.REACT_APP_BASE_URL}`;
+// console.log()
 class roomService {
-    async addRoom(payload){
-        try{
-            // const response = axios(`${baseUrl}/addRoom`, roomDetails)
-            // return response
-            console.log("room details", payload);
-        }catch (err){
-            throw err.message
-        }
+  async getAllRooms() {
+    try {
+      const response = await axios.get(`${baseUrl}/getAllRooms`);
+      return response;
+    } catch (err) {
+      throw err.message;
     }
+  }
+  async addRoom(payload) {
+    try {
+      const response = await axios.post(`${baseUrl}/addRoom`, payload);
+      return response;
+    } catch (err) {
+      throw err.message;
+    }
+  }
 }
 
 export default new roomService();
